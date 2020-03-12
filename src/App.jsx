609d703fef5890ace309d98a7fed1f4f6ec9f616
Anish2504 <anish.kanswal@gmail.com>
2020-03-12 09:56:44 +0100

    changed to an arrow function in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
     <>
-    <h1 id="header">Grog-Bot</h1>
+      <h1>Grog-Bot</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
