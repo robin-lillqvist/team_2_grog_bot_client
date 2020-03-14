4ad335cec8722fb6253b10dbb56b1230b7c9ddde
bounce <johan@waytogo.se>
2020-03-14 16:18:13 +0100

    Making successfull GET request for specific cocktail. To be continued...

--- src/components/SelectIngredients.jsx
+++ src/components/SelectIngredients.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 class SelectIngredients extends Component {
   state = {
     selectedIngredient: "",
-    cocktailList: []
+    cocktailList: [],
+    selectedCocktail: []
   };
 
   setIngredient(event) {
@@ -23,13 +24,25 @@ class SelectIngredients extends Component {
     });
   }
 
+  async getDetails(event) {
+    let id = event.target.dataset.id;
+    debugger
+    let result = await axios.get(
+      `/cocktails/${id}`
+    );
+    debugger
+    // this.setState({
+    //   selectedCocktail: result.data
+    // })
+    }
+
   render() {
     let cocktailIndex;
     if (this.state.cocktailList !== []) {
       cocktailIndex = this.state.cocktailList.map(cocktail => {
         return (
           <li>
-            <button key={cocktail.idDrink}>{cocktail.strDrink}</button>
+            <button key={cocktail.idDrink} data-id={cocktail.idDrink} onClick={this.getDetails.bind(this)}>{cocktail.strDrink}</button>
           </li>
         );
       });
